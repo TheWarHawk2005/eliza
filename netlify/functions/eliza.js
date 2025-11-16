a12706300dd741f1c4a8378eecc4395a3db51d1b
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2025-11-15 23:07:36 -0500

    Update eliza.js

--- netlify/functions/eliza.js
+++ netlify/functions/eliza.js
@@ -1,8 +1,8 @@
-exports.handler = async (event, context) => {
+izexports.handler = async (event, context) => {
   const data = JSON.parse(event.body || "{}");
-  const version = {version: "0.0.0"}
+  const version = "0.0.0"
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Hello my name is Shannon :)", version, data })
+    body: JSON.stringify({ message: "Hello my name is Eliza :)", version, data })
   };
 };
