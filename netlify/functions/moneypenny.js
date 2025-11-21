7647e60c3900914045ef82333664191a33ea5d0c
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2025-11-20 22:25:26 -0500

    Update moneypenny.js

--- netlify/functions/moneypenny.js
+++ netlify/functions/moneypenny.js
@@ -234,7 +234,7 @@ exports.handler = async (event, context) => {
                             From: { Email: "616strength@616strength.com", Name: "616 Strength & Nutrition" },
                             To: [{ Email: "louis.h.harrison@gmail.com", Name: "Louis H" }],
                             Subject: `New Message from ${formName}`,
-                            TemplateID: 7511636,
+                            TemplateID: 7511790,
                             TemplateLanguage: true,
                             Variables: templateVariables
                         }
